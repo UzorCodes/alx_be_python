5746cc9ede27fbc560e9b064e546e3907b7f1519
Paul <paulezumezu@gmail.com>
2025-07-13 11:38:17 +0100

    intial commit to add first python script

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+# This code person basic arithmetic operations of two numbers
+number1 = 10  # defines value for first variable
+number2 = 5   # defines value for second variable
+
+
+# Performs the basic operations
+sum = number1 + number2  
+difference = number1 - number2 
+product = number1 * number2  
+
+
+# Outputs result of the basic operations
+print("Addition of", number1, "and", number2, "is" sum) 
+print("Difference of", number1, "and", number2, "is" difference) 
+print("Product of", number1, "and", number2, "is" product) 
