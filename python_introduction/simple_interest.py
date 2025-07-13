c64b6f38d8e23fe76706cba6a133719262711e95
Paul <paulezumezu@gmail.com>
2025-07-13 11:56:23 +0100

    Update with simple_interest script to correct some errors

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,12 +1,12 @@
 # This code calculates the simple interest earned over a period of time
  
-principal 1000  # defines value for principle amount
+principal = 1000 # defines value for principle amount
 rate = 0.05   # defines rate of interest per annum
 time = 3  # defines the time period
 
 
 # Calculate the simple interest
-simple_interest = principal rate * time 
+simple_interest = principal *rate * time
  
 
 # Outputs result of the calculation
