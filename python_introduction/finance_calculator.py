50831f0d7be8176a59360d955d44b2eca4fb69e1
Paul <paulezumezu@gmail.com>
2025-07-13 12:33:47 +0100

    Update with finance_calculator.py script

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,17 @@
+# This code calculates a user's monthly saving and an expected compound interest in a year
+# The current year is assumed to be 2023 and the future year is 2050
+ 
+monthly_income = int(input("Enter your monthly income: ")) # prompts user to enter monthly income
+total_monthly_expense = int(input("Enter your total monthly expense: ")) # prompts user to enter total monthly expense
+
+
+#Calculates monthly savings
+monthly_savings = monthly_income - total_monthly_expense
+
+
+#calculates compound interest in a year at an assumed rate of 5% per annum
+projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+
+# Outputs result of the calculation
+print("Your monthly savings is $", monthly_savings)
+print("Projected savings after one year, with interest, is: $", projected_savings)
