4520238bbb2da50fb36a5a7fb86710f908febff6
Paul <paulezumezu@gmail.com>
2025-07-20 17:35:59 +0100

    New correction to multiplication_table.py script

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,7 +1,7 @@
 # This code creates a multiplication table
 
-num1 = int(input("Enter a number to see its multiplication table: "))  # prompts user to enter number to get multiplication table.
+number = int(input("Enter a number to see its multiplication table: "))  # prompts user to enter number to get multiplication table.
 
 for x in range(1,11):
-    product = num1 * x
-    print("{num1} * {x} = {product}", end="\t")
+    product = number * x
+    print(f"{number} * {x} = {product}", end="\t")
