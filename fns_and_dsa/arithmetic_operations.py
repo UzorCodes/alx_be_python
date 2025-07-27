0f4e8967e8771427dde3dabc3388e1118e272877
Paul <paulezumezu@gmail.com>
2025-07-27 20:59:46 +0100

    new edit file arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -3,7 +3,7 @@
 def perform_operation(num1, num2, operation):
     num1 = float(num1)
     num2 = float(num2)
-    operation = str(operation)
+    operation = operation.lower()
     match operation:
         case "add":
             return num1 + num2
@@ -12,7 +12,7 @@ def perform_operation(num1, num2, operation):
         case "multiply":
             return num1 * num2
         case "divide":
-            if num2 == 0:
-                print("Cannot divide by zero.")
-            else:
+            if num2 != 0:
                 return num1 / num2
+            elif num2 == 0:
+                print("Cannot divide by zero.")
