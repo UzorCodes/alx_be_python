eab5b8804930afa12c1a4d7c2cf6cdff298165ec
Paul <paulezumezu@gmail.com>
2025-08-16 18:23:43 +0100

    add new scripts

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,12 @@
+class BankAccount:
+    def __init__(self, account_balance):
+        self.account_balance = 0 # Default attribute value
+
+    def deposit(self, amount):
+        self.account_balance += amount
+
+    def withdraw(self, amount):
+        self.account_balance -= amount    
+
+    def display_balance():
+        print("Current Balance: ${amount}",
