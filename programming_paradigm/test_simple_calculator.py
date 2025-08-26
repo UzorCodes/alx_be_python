f1691e7ae913eafdb470bd1b04b6a8fb0b64ee6d
Paul <paulezumezu@gmail.com>
2025-08-26 12:02:17 +0100

    Test script

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,34 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        """Set up the SimpleCalculator instance before each test."""
+        self.calc = SimpleCalculator()
+
+    def test_addition(self):
+        """Test the addition method."""
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        
+
+
+    def test_subtraction(self):
+        """Test the subtraction method."""
+        self.assertEqual(self.calc.subtract(6, 3), 3)
+        self.assertEqual(self.calc.subtract(2, 5), -3)
+       
+
+
+    def test_multiplication(self):
+        """Test the multiplcation method."""
+        self.assertEqual(self.calc.multiply(2, 3), 6)
+        self.assertEqual(self.calc.multiply(4, 1), 1)
+
+
+    def test_division(self):
+        """Test the divison method."""
+        self.assertEqual(self.calc.divide(10, 2, 5)
+        self.assertEqual(self.calc.divide(4, 0), None)
+ 
