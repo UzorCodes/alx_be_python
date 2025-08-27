fafb086f94a2ed61e3906eaade4f095052d6de9f
Paul <paulezumezu@gmail.com>
2025-08-27 11:41:23 +0100

    first commit library management

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,20 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = 0  # Default attribute value
+
+    def list_available_books(self, book, author):
+        for i in 
+
+    def add_book(self, title, author):
+
+        .append()
+        
+
+    def check_out_book(self):
+        .remove()
+        
+
+    def return_book(self, ):
+       
